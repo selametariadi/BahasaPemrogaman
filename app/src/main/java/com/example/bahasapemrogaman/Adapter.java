bd9f2dbdf27ba0e2db963701c310e83a47d308c3
selametariadi <ariadisela1230@gmail.com>
2021-12-25 17:04:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bahasapemrogaman/Adapter.java
@@ -0,0 +1,53 @@
+package com.example.bahasapemrogaman;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class Adapter extends ArrayAdapter {
+    Context context;
+    ArrayList<Data> datas;
+    LayoutInflater layoutInflater;
+
+    ImageView logo;
+    TextView title, deskripsi, helo_word, detail_web;
+
+    public Adapter(Context context, ArrayList<Data> data) {
+        super(context, R.layout.list_algoritma, data);
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+        this.datas = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = layoutInflater.inflate(R.layout.list_algoritma, parent, false);
+        Data data = datas.get(position);
+
+        logo = convertView.findViewById(R.id.logo);
+        title = convertView.findViewById(R.id.NamePerograman);
+        helo_word = convertView.findViewById(R.id.heloWord);
+        deskripsi = convertView.findViewById(R.id.deskripsi);
+        detail_web = convertView.findViewById(R.id.detailJava);
+
+        Glide
+                .with(context)
+                .load(data.getLogo())
+                .placeholder(R.drawable.ic_refresh_foreground)
+                .into(logo);
+        title.setText(data.getNameAlgoritma());
+        deskripsi.setText(data.getDescription());
+        helo_word.setText(data.getHello_world());
+        detail_web.setText(data.getBaca_lebih_lanjut());
+
+        return convertView;
+    }
+}
